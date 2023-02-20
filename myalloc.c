65dd01d0f68519208b822657ca637fa0c1abc698
Xander Bailey <xander.bailey@gmail.com>
2023-02-19 16:26:28 -0800

    Add print_data

--- myalloc.c
+++ myalloc.c
@@ -40,9 +40,38 @@ void* myalloc(int size) {
 }
 
 
+void print_data(void)
+{
+    struct block *b = head;
+
+    if (b == NULL) {
+        printf("[empty]\n");
+        return;
+    }
+
+    while (b != NULL) {
+        // Uncomment the following line if you want to see the pointer values
+        //printf("[%p:%d,%s]", b, b->size, b->in_use? "used": "free");
+        printf("[%d,%s]", b->size, b->in_use? "used": "free");
+        if (b->next != NULL) {
+            printf(" -> ");
+        }
+
+        b = b->next;
+    }
+
+    printf("\n");
+}
+
+
 int main(void)
 {
-    int* test = myalloc(64);
+    void* test;
+
+    print_data();
+    test = myalloc(64);
+    print_data();
+    test = myalloc(64);
     printf("%p\n", test);
 }
 
